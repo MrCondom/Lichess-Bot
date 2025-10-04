ad8e4940e78dcaab9b792bf1db0fce93e7e0207b
JACKSON CHINEDU <chiefjacksonian@gmail.com>
2025-10-05 00:23:20 +0100

    updated github withengines

---
+++ run_bot.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+:loop
+echo Starting Lichess Bot...
+"C:\Users\JACKSON CHINEDU\AppData\Local\Microsoft\WindowsApps\python.exe" "C:\Users\JACKSON CHINEDU\lichess-bot\lichess-bot.py"
+echo Bot stopped or crashed. Restarting in 30 seconds...
+timeout /t 30 /nobreak
+goto loop
